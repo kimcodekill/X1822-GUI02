51b191f5510d8f4cf9e018dfcd962310331d55fd
Joakim Linna <joakimlinna1998@gmail.com>
2018-02-02 18:15:42 +0100

    added gui link

--- X1822-GUI02/index.js
+++ X1822-GUI02/index.js
@@ -1,9 +1,10 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       document.getElementById("notes").innerHTML = xhttp.responseText;
+        // Typical action to be performed when the document is ready:
+        document.getElementById("notes").innerHTML = xhttp.responseText;
     }
 };
-xhttp.open("GET", "http://localhost:8088/notes/5a74680ec0e3871a7857e341", true);
+//xhttp.open("GET", "http://localhost:8088/notes/5a74680ec0e3871a7857e341", true);
+xhttp.open("GET", "http://localhost:8088/notes", true);
 xhttp.send();
