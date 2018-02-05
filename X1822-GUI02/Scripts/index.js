299c1a1dbc6b93e8f3295ddfedd787e4d9fe0d9a
Joakim <themanzed@gmail.com>
2018-02-05 13:42:45 +0100

    replaced local ko w/ cdn, added bindings

--- X1822-GUI02/Scripts/index.js
+++ X1822-GUI02/Scripts/index.js
@@ -1,19 +1,19 @@
+var inData = "";
+
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         // Typical action to be performed when the document is ready:
-        var notes = JSON.parse(xhttp.response);
-        for (i = 0; i < notes.length; i++) {
-            var note = notes[i];
-            document.getElementById('dropdown').innerHTML += '<option value="' + JSON.stringify(note._id) +
-                '">' + JSON.stringify(note.title) + '</option>';
-        }
-        //document.getElementById('notes').innerHTML = xhttp.response;
+        inData = JSON.parse(xhttp.response);
+
+        console.log(ko);
+        ko.applyBindings(new viewModel(inData));
     };
 };
 xhttp.open("GET", "http://localhost:8088/notes", true);
 xhttp.send();
 
-function showNote(id) {
-
-};
+var viewModel = function (data) {
+    this.notes = ko.observableArray(data);
+    
+}
