59b5533b3027b0999635efcd2ddd99770519142a
Joakim <themanzed@gmail.com>
2018-02-05 15:09:08 +0100

    added edit-area for note

--- X1822-GUI02/Scripts/index.js
+++ X1822-GUI02/Scripts/index.js
@@ -1,12 +1,11 @@
-var inData = "";
+
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         // Typical action to be performed when the document is ready:
-        inData = JSON.parse(xhttp.response);
+        var inData = JSON.parse(xhttp.response);
 
-        console.log(ko);
         ko.applyBindings(new viewModel(inData));
     };
 };
@@ -15,5 +14,4 @@ xhttp.send();
 
 var viewModel = function (data) {
     this.notes = ko.observableArray(data);
-    
 }
